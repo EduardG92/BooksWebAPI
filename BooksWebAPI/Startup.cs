675dcaadabe6a9f29b90d267a9f2ee4d7fc84cbf
sal frt <ticusanvladut22032002@gmail.com>
2022-07-26 11:41:36 +0300

    ConnectionString in app settings, adding Startup

---
+++ BooksWebAPI/Startup.cs
@@ -0,0 +1,37 @@
+﻿using BooksWebAPI.Contexts;
+using Microsoft.EntityFrameworkCore;
+
+namespace BooksWebAPI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        public IConfiguration Configuration { get; }
+        public static void ConfigureServices(WebApplicationBuilder builder)
+        {
+            var connectionString = builder.Configuration["ConnectionStrings:MedITConnectionString"];
+            builder.Services.AddDbContext<MedITContext>(o => o.UseSqlServer(connectionString));
+
+            builder.Services.AddControllers();
+        }
+
+        public static void Configure(WebApplication app)
+        {
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+        }
+    }
+}
