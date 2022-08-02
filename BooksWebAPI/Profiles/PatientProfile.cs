e6a2dd6215ea5355b2d783783f9da288f6ce8d37
sal frt <ticusanvladut22032002@gmail.com>
2022-08-02 15:46:35 +0300

    Added  Maps

---
+++ BooksWebAPI/Profiles/PatientProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+
+namespace BooksWebAPI.Profiles
+{
+    public class PatientProfile : Profile
+    {
+        public PatientProfile()
+        {
+            CreateMap<Entities.Patient, ExternalModels.PatientDTO>();
+            CreateMap<ExternalModels.PatientDTO, Entities.Patient>();
+        }
+    }
+}
---
+++ BooksWebAPI/Profiles/TreatmentProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+
+namespace BooksWebAPI.Profiles
+{
+    public class TreatmentProfile : Profile
+    {
+        public TreatmentProfile()
+        {
+            CreateMap<Entities.Doctor, ExternalModels.DoctorDTO>();
+            CreateMap<ExternalModels.DoctorDTO, Entities.Doctor>();
+
+            CreateMap<Entities.Treatment, ExternalModels.TreatmentDTO>();
+            CreateMap<ExternalModels.TreatmentDTO, Entities.Treatment>();
+
+        }
+    }
+}
